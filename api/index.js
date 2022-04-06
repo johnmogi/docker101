28030d3022bf7fd497f94656da0a373ab2dcd3f3
johnmogi <anguru@gmail.com>
2022-04-06 10:43:01 +0300

    reorder

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const { port, host } = require("./src/config");
+const { connectDb } = require("./src/helpers/db");
 const app = express();
-const mongoose = require("mongoose");
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+
 app.get("/test", (req, res) => {
   res.send("all correct");
 });
-// insert db connection
-mongoose.connect("mongodb://localhost:27017/docker101");
 
-// app.listen(PORT);
-// console.log(`started at http://localhost:${PORT}, destination: ${HOST}`);
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`started at http://localhost:${port}, destination: ${host}`);
+  });
+};
+connectDb()
+  .on("error", console.log())
+  .on("disconnected", connectDb)
+  .once(startServer);
